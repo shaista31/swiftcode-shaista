93f35ef6b23baa8e64d06e6089428e4f7b72b737
shaista31 <shaistafathima31@gmail.com>
2018-04-14 12:13:16 +0000

    Added EditedFrontEnd

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,5 +2,22 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
 
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+},
+        {
+            'sender': 'BOT',
+            'text': 'What can i do for you?'
+},
+        {
+            'sender': 'USER',
+            'text': 'What is the date today?'
+},
+        {
+            'sender': 'BOT',
+            'text': 'Todays date is 14 - 04 - 18 '
+}
+];
 });
